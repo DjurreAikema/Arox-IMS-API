bb618480e2a96927197630619f97ece89f3e6ffe
d.aikema <djurreaikema@gmail.com>
2024-09-27 15:36:20 +0200

    ToolOutput endpoints

--- AROX.IMS.API/Endpoints/ToolOutputEndpoints.cs
+++ AROX.IMS.API/Endpoints/ToolOutputEndpoints.cs
@@ -1,4 +1,9 @@
-﻿using GeneralTools.AspNetCore.MinimalApi;
+﻿using AROX.IMS.API.Classes;
+using AROX.IMS.API.Exceptions;
+using AROX.IMS.API.Services;
+using GeneralTools.AspNetCore.MinimalApi;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 
 namespace AROX.IMS.API.Endpoints;
@@ -7,5 +12,81 @@ public class ToolOutputEndpoints : IMapEndpoints
 {
     public static void MapEndpoints(IEndpointRouteBuilder app)
     {
+        // Get all tool outputs
+        app.MapGet("api/tooloutputs", async (ToolOutputService toolOutputService) =>
+                Results.Ok((object?) await toolOutputService.GetToolOutputs()))
+            .WithTags("ToolOutput");
+
+        // Get tool output by id
+        app.MapGet("api/tooloutputs/{id:long}", async (ToolOutputService toolOutputService, long id) =>
+            {
+                try
+                {
+                    var toolOutput = await toolOutputService.GetToolOutput(id);
+                    return toolOutput == null ? Results.NotFound() : Results.Ok(toolOutput);
+                }
+                catch (Exception e)
+                {
+                    return Results.Problem(e.Message);
+                }
+            })
+            .WithTags("ToolOutput");
+
+        // Add new tool output
+        app.MapPost("api/tooloutputs", async (ToolOutputService toolOutputService, NewToolOutputDto toolOutput) =>
+            {
+                try
+                {
+                    var result = await toolOutputService.AddToolOutput(toolOutput);
+                    return Results.Created("api/tooloutputs", result);
+                }
+                catch (NotFoundException ex)
+                {
+                    return Results.NotFound(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            })
+            .WithTags("ToolOutput");
+
+        // Update tool output
+        app.MapPut("api/tooloutputs", async (ToolOutputService toolOutputService, ToolOutputDto toolOutput) =>
+            {
+                try
+                {
+                    var result = await toolOutputService.UpdateToolOutput(toolOutput);
+                    return Results.Ok(result);
+                }
+                catch (NotFoundException ex)
+                {
+                    return Results.NotFound(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            })
+            .WithTags("ToolOutput");
+
+        // Delete tool output
+        app.MapDelete("api/tooloutputs/{id:long}", async (ToolOutputService toolOutputService, long id) =>
+            {
+                try
+                {
+                    var result = await toolOutputService.DeleteToolOutput(id);
+                    return Results.Ok(result);
+                }
+                catch (NotFoundException ex)
+                {
+                    return Results.NotFound(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            })
+            .WithTags("ToolOutput");
     }
 }
