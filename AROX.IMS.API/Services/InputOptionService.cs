f6b889e3508c59070045e6347fc92b99dcf1e273
d.aikema <djurreaikema@gmail.com>
2024-09-27 15:21:14 +0200

    InputOption service

--- AROX.IMS.API/Services/InputOptionService.cs
+++ AROX.IMS.API/Services/InputOptionService.cs
@@ -1,8 +1,72 @@
-﻿using IMS.EF.Models;
+﻿using AROX.IMS.API.Classes;
+using AROX.IMS.API.Exceptions;
+using AROX.IMS.API.Helpers;
+using IMS.EF.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AROX.IMS.API.Services;
 
 public class InputOptionService(AROX_IMSContext context)
 {
-    
+    // Get all input options
+    public async Task<List<InputOptionDto>> GetInputOptions()
+    {
+        return await context.InputOptions
+            .Select(x => InputOptionConverters.ToModel(x))
+            .ToListAsync();
+    }
+
+    // Get input option by id
+    public async Task<InputOptionDto?> GetInputOption(long id)
+    {
+        return await context.InputOptions
+            .Where(x => x.Id == id)
+            .Select(x => InputOptionConverters.ToModel(x))
+            .FirstOrDefaultAsync();
+    }
+
+    // Add new input option
+    public async Task<InputOptionDto> AddInputOption(NewInputOptionDto inputOption)
+    {
+        // Validate
+        await NotFoundException.EnsureToolInputExists(context, inputOption.InputId);
+
+        // Add
+        var newInputOption = InputOptionConverters.ToEntity(inputOption);
+        context.InputOptions.Add(newInputOption);
+        await context.SaveChangesAsync();
+
+        // Return
+        return InputOptionConverters.ToModel(newInputOption);
+    }
+
+    // Update input option
+    public async Task<InputOptionDto> UpdateInputOption(InputOptionDto inputOption)
+    {
+        // Validate
+        var existingInputOption = await NotFoundException.EnsureInputOptionExists(context, inputOption.Id);
+        await NotFoundException.EnsureToolInputExists(context, inputOption.InputId);
+
+        // Update
+        InputOptionConverters.UpdateEntity(existingInputOption, inputOption);
+        context.Entry(existingInputOption).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+
+        // Return
+        return InputOptionConverters.ToModel(existingInputOption);
+    }
+
+    // Delete input option
+    public async Task<InputOptionDto> DeleteInputOption(long id)
+    {
+        // Validate
+        var existingInputOption = await NotFoundException.EnsureInputOptionExists(context, id);
+
+        // Delete
+        context.InputOptions.Remove(existingInputOption);
+        await context.SaveChangesAsync();
+
+        // Return
+        return InputOptionConverters.ToModel(existingInputOption);
+    }
 }
